46707e9abfeec69e9d967818c442d1bf38530f53
Nikita <fk724s@mail.ru>
2024-11-15 23:16:57 +0300

    add usage example

--- main.c
+++ main.c
@@ -1,36 +1,70 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdbool.h>
+#include "utils.h"
+#include "api.h"
 
-// int lab2_open(const char *path);
-// int lab2_close(int fd);
-// ssize_t lab2_read(int fd, void *buf, size_t count);
-// ssize_t lab2_write(int fd, const void *buf, size_t count);
-// off_t lab2_lseek(int fd, off_t offset, int whence);
-// int lab2_fsync(int fd);
-
-typedef struct cache_block cache_block;
-typedef struct file_cache file_cache;
-
-struct cache_block {
-    off_t offset;
-    void *data;
-    size_t block_size;
-    int usage_frequency;
-    bool dirty;
-    cache_block *next_block;
-};
-
-struct file_cache {
-    int fd;
-    size_t block_size;
-    size_t max_blocks;
-    size_t current_blocks;
-    cache_block *head;
-};
+#define BLOCK_SIZE 4096
+#define MAX_BLOCKS 4
 
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+int main() {
+    const char *file_path = "../test.txt";
+
+    file_cache *cache = lab2_open(file_path, BLOCK_SIZE, MAX_BLOCKS);
+    if (!cache) {
+        fprintf(stderr, "[!] Init cache\n");
+        return EXIT_FAILURE;
+    }
+
+    printf("[+] Init cache\n");
+
+
+    const char *write_data = "Hello, World!";
+    const ssize_t data_len = (ssize_t) strlen(write_data);
+
+    if (lab2_write(cache, write_data, data_len, 0) < 0) {
+        fprintf(stderr, "[!] Write data\n");
+        lab2_close(cache);
+
+        return EXIT_FAILURE;
+    }
+
+    printf("[+] Write data\n");
+    printf("Data written: %s\n", write_data);
+
+
+    if (lab2_fsync(cache) < 0) {
+        fprintf(stderr, "[!] Synchronize cache with disk\n");
+        lab2_close(cache);
+
+        return EXIT_FAILURE;
+    }
+
+    printf("[+] Synchronize data with disk\n");
+
+
+    char read_data[1024];
+    memset(read_data, 0, sizeof(read_data));
+
+    if (lab2_read(cache, read_data, data_len, 0) < 0) {
+        fprintf(stderr, "[!] Read data\n");
+        lab2_close(cache);
+        return EXIT_FAILURE;
+    }
+
+    printf("[+] Read data\n");
+    printf("Data read: %s\n", read_data);
+
+
+    if (lab2_lseek(cache, 1024, SEEK_SET) < 0) {
+        perror("[!] Seek to position");
+        lab2_close(cache);
+        return EXIT_FAILURE;
+    }
+
+    printf("[+] Seek to position\n");
+
+
+    lab2_close(cache);
+    printf("[+] Close cache\n");
+
+    return EXIT_SUCCESS;
 }
